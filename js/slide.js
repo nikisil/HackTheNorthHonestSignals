7b8fe7c7676785d08e3607f3cb9c7804c6ffcbb5
dgandhi98 <divyanshugandhi@yahoo.com>
2017-09-16 23:14:35 -0400

    recorder functionality

--- js/slide.js
+++ js/slide.js
@@ -1,9 +1,13 @@
-/*
-  Taken from https://mozilla.github.io/pdf.js/examples/
-*/
 // If absolute URL from the remote server is provided, configure the CORS
 // header on that server.
-var url = './helloworld.pdf';
+var url = './helloworld3.pdf';
+
+var audioArray = [];
+var recorder;
+var context = new AudioContext();
+
+window.URL = window.URL || window.webkitURL;
+navigator.getUserMedia  = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
 // The workerSrc property shall be specified.
 PDFJS.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
@@ -62,19 +66,6 @@ function queueRenderPage(num) {
   }
 }
 
-
-/**
- * Displays next page.
- */
-function onNextPage() {
-  if (pageNum >= pdfDoc.numPages) {
-    return;
-  }
-  pageNum++;
-  queueRenderPage(pageNum);
-}
-document.getElementById('next').addEventListener('click', onNextPage);
-
 /**
  * Asynchronously downloads PDF.
  */
@@ -85,3 +76,81 @@ PDFJS.getDocument(url).then(function(pdfDoc_) {
   // Initial/first page rendering
   renderPage(pageNum);
 });
+
+/**
+ * Displays next page.
+ */
+function onNextPage() {
+  stopSlideRecording();
+  console.log(pageNum + " " + pdfDoc.numPages);
+  if (pageNum >= pdfDoc.numPages) {
+    document.getElementById('next').disabled = true;
+    console.log("Sending to Python Backend");
+    console.log(audioArray);
+    var form = new FormData();
+    form.append('file', audioArray[0], 'one.wav');
+    var xmlhttp=new XMLHttpRequest();
+    var url = "accessdata";
+    xmlhttp.open("POST",url,true);
+    xmlhttp.send(form);
+
+    /*$.ajax({
+      type: 'POST',
+      url: 'ServerURL',
+      data: form,
+      cache: false,
+      processData: false,
+      contentType: false
+    }).done(function(data){
+      console.log("Sent");
+      console.log(data);
+    })*/
+    //need to ajax to python from here...
+    return;
+  }
+  else if(pageNum == pdfDoc.numPages-1) {
+    document.getElementById('next').textContent = "Submit";
+  }
+  else {
+    startSlideRecording();
+  }
+  pageNum++;
+  queueRenderPage(pageNum);
+}
+document.getElementById('next').addEventListener('click', onNextPage);
+
+var onSuccess = function(s) {
+    var mediaStreamSource = context.createMediaStreamSource(s);
+    console.log(mediaStreamSource);
+    recorder = new Recorder(mediaStreamSource);
+    recorder.record();
+    // audio loopback
+    // mediaStreamSource.connect(context.destination);
+}
+
+var onFail = function(e) {
+  console.log('Rejected!', e);
+};
+
+function startSlideRecording() {
+  if (navigator.getUserMedia) {
+    navigator.getUserMedia({audio: true}, onSuccess, onFail);
+  } else {
+    console.log('navigator.getUserMedia not present');
+  }
+}
+
+function stopSlideRecording() {
+  recorder.stop();
+  recorder.exportWAV(function(s) {
+    console.log(s);
+    audioArray.push(s);
+    var wUrl = window.URL.createObjectURL(s);
+    //audioArray.push(wUrl);
+    console.log(audioArray);
+    //audio.src = wUrl;
+  });
+}
+
+// on load
+startSlideRecording();
